91233b4fccabed95da8179dbe3aaa0a7c64f8485
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2018-05-17 21:16:15 +0200

    added some missing ones

--- JellyDust.Dapper/DapperExtensions.cs
+++ JellyDust.Dapper/DapperExtensions.cs
@@ -10,20 +10,39 @@ namespace JellyDust.Dapper
         public static async Task<int> ExecuteAsync(this ITransaction @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
             await @this.DbTransaction.Connection.ExecuteAsync(sql, param, @this.DbTransaction, commandTimeout, commandType);
 
+        public static async Task<IDataReader> ExecuteReaderAsync(this ITransaction @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
+           await @this.DbTransaction.Connection.ExecuteReaderAsync(sql, param, @this.DbTransaction, commandTimeout, commandType);
+
+        public static async Task<object> ExecuteScalarAsync(this ITransaction @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
+            await @this.DbTransaction.Connection.ExecuteScalarAsync(sql, param, @this.DbTransaction, commandTimeout, commandType);
+
+        public static async Task<T> ExecuteScalarAsync<T>(this ITransaction @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
+            await @this.DbTransaction.Connection.ExecuteScalarAsync<T>(sql, param, @this.DbTransaction, commandTimeout, commandType);
+
+
         public static async Task<IEnumerable<T>> QueryAsync<T>(this ITransaction @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
             await @this.DbTransaction.Connection.QueryAsync<T>(sql, param, @this.DbTransaction, commandTimeout, commandType);
 
         public static async Task<T> QueryFirstAsync<T>(this ITransaction @this, string sql, object param = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null) =>
             await @this.DbTransaction.Connection.QueryFirstAsync<T>(sql, param, @this.DbTransaction, commandTimeout, commandType);
 
+        public static async Task<T> QueryFirstOrDefaultAsync<T>(this ITransaction @this, string sql, object param = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null) =>
+            await @this.DbTransaction.Connection.QueryFirstOrDefaultAsync<T>(sql, param, @this.DbTransaction, commandTimeout, commandType);
+
         public static async Task<SqlMapper.GridReader> QueryMultipleAsync(this ITransaction @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
             await @this.DbTransaction.Connection.QueryMultipleAsync(sql, param, @this.DbTransaction, commandTimeout, commandType);
 
-        public static async Task<T> ExecuteScalarAsync<T>(this ITransaction @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
-            await @this.DbConnection.ExecuteScalarAsync<T>(sql, param, @this.DbTransaction, commandTimeout, commandType);
 
+
+        
         public static async Task<int> ExecuteAsync(this IDbTransaction @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
-            await @this.Connection.ExecuteAsync(sql, param, @this, commandTimeout, commandType);
+         await @this.Connection.ExecuteAsync(sql, param, @this, commandTimeout, commandType);
+
+        public static async Task<IDataReader> ExecuteReaderAsync(this IDbTransaction @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
+           await @this.Connection.ExecuteReaderAsync(sql, param, @this, commandTimeout, commandType);
+
+        public static async Task<object> ExecuteScalarAsync(this IDbTransaction @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
+            await @this.Connection.ExecuteScalarAsync(sql, param, @this, commandTimeout, commandType);
 
         public static async Task<T> ExecuteScalarAsync<T>(this IDbTransaction @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
             await @this.Connection.ExecuteScalarAsync<T>(sql, param, @this, commandTimeout, commandType);
@@ -34,8 +53,25 @@ namespace JellyDust.Dapper
         public static async Task<T> QueryFirstAsync<T>(this IDbTransaction @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
             await @this.Connection.QueryFirstAsync<T>(sql, param, @this, commandTimeout, commandType);
 
+        public static async Task<SqlMapper.GridReader> QueryMultipleAsync(this IDbTransaction @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
+          await @this.Connection.QueryMultipleAsync(sql, param, @this, commandTimeout, commandType);
+
+        public static async Task<T> QueryFirstOrDefaultAsync<T>(this IDbTransaction @this, string sql, object param = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null) =>
+            await @this.Connection.QueryFirstOrDefaultAsync<T>(sql, param, @this, commandTimeout, commandType);
+
+
+
         public static async Task<int> ExecuteAsync(this IConnection @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
-            await @this.DbConnection.ExecuteAsync(sql, param, @this.GetCurrentDbTransaction(), commandTimeout, commandType);
+         await @this.DbConnection.ExecuteAsync(sql, param, @this.GetCurrentDbTransaction(), commandTimeout, commandType);
+
+        public static async Task<IDataReader> ExecuteReaderAsync(this IConnection @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
+           await @this.DbConnection.ExecuteReaderAsync(sql, param, @this.GetCurrentDbTransaction(), commandTimeout, commandType);
+
+        public static async Task<object> ExecuteScalarAsync(this IConnection @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
+            await @this.DbConnection.ExecuteScalarAsync(sql, param, @this.GetCurrentDbTransaction(), commandTimeout, commandType);
+
+        public static async Task<T> ExecuteScalarAsync<T>(this IConnection @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
+            await @this.DbConnection.ExecuteScalarAsync<T>(sql, param, @this.GetCurrentDbTransaction(), commandTimeout, commandType);
 
         public static async Task<SqlMapper.GridReader> QueryMultipleAsync(this IConnection @this, string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null) =>
             await @this.DbConnection.QueryMultipleAsync(sql, param, @this.GetCurrentDbTransaction(), commandTimeout, commandType);
@@ -45,5 +81,8 @@ namespace JellyDust.Dapper
 
         public static async Task<T> QueryFirstAsync<T>(this IConnection @this, string sql, object param = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null) =>
             await @this.DbConnection.QueryFirstAsync<T>(sql, param, @this.GetCurrentDbTransaction(), commandTimeout, commandType);
+
+        public static async Task<T> QueryFirstOrDefaultAsync<T>(this IConnection @this, string sql, object param = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null) =>
+           await @this.DbConnection.QueryFirstOrDefaultAsync<T>(sql, param, @this.GetCurrentDbTransaction(), commandTimeout, commandType);
     }
 }
